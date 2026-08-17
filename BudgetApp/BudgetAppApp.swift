a64cbd124544e32e5aaeef880ca869655f6ef1e4
Guofeng Luo <luoguofeng0401@gmail.com>
2026-08-17 11:29:29 +0800

    Initial Commit

---
+++ BudgetApp/BudgetAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BudgetAppApp.swift
+//  BudgetApp
+//
+//  Created by Guofeng Luo on 2026/8/17.
+//
+
+import SwiftUI
+
+@main
+struct BudgetAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
